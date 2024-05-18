61e5bcfde79ebab469fcc3093d1e52644b8b0b01
nicodecillis <nicolodecillis@gmail.com>
2024-05-18 13:02:52 +0200

    Raggruppati content ratings e categorie e aggiunti alcuni dati mancanti

--- cleaning.py
+++ cleaning.py
@@ -4,6 +4,26 @@ from google_play_scraper import app
 import google_play_scraper.exceptions
 
 
+def group_categories(df):
+    df['Category'] = df['Category'].replace(['Action', 'Adventure', 'Arcade', 'Casino', 'Card', 'Casual', 'Educational',
+                                             'Music', 'Puzzle', 'Racing', 'Role Playing', 'Simulation', 'Strategy',
+                                             'Trivia', 'Word', 'Board'], 'Games')
+    df['Category'] = df['Category'].replace(['Art & Design', 'Photography'], 'Creativity')
+    df['Category'] = df['Category'].replace(['Books & Reference', 'Comics', 'News & Magazines'], 'Reads')
+    df['Category'] = df['Category'].replace(['Business', 'Finance'], 'Finance')
+    df['Category'] = df['Category'].replace(['Communication', 'Social'], 'Communication')
+    df['Category'] = df['Category'].replace(['Health & Fitness', 'Medical', 'Sports'], 'Health & Fitness')
+    df['Category'] = df['Category'].replace(['Maps & Navigation', 'Travel & Local'], 'Travel & Navigation')
+    df['Category'] = df['Category'].replace(['Tools', 'Libraries & Demo'], 'Tools')
+    df['Category'] = df['Category'].replace(['Productivity', 'Video Players & Editors'], 'Productivity')
+
+
+def group_content_rating(df):
+    df['Content Rating'] = df['Content Rating'].replace(['Everyone', 'Unrated'], 'Everyone')
+    df['Content Rating'] = df['Content Rating'].replace(['Everyone 10+', 'Teen'], 'Teen')
+    df['Content Rating'] = df['Content Rating'].replace(['Mature 17+', 'Adults only 18+'], 'Adults')
+
+
 def contains_foreign_characters(text):
     text = str(text)
     max_ascii_range = 255
@@ -78,10 +98,10 @@ def clean(dataset):
     df.loc[df["App Id"] == "clumsy.cheatingdevz.com.clumsybirdcheat", "Content Rating"] = "Everyone"
     df.loc[df["App Id"] == "com.jb.gosms.pctheme.moji", "Content Rating"] = "Everyone"
 
-    df.loc[df["Content Rating"] == "Teen", "Content Rating"] = "Teen 13+"
-    df.loc[df["Content Rating"] == "Adults only 18+", "Content Rating"] = "Mature 17+"
-
-
+    # raggruppamento delle Categorie
+    group_categories(df)
+    # raggruppamento dei Content Rating
+    group_content_rating(df)
     df.to_csv("dataset/clean-playstore-apps.csv", index=False)
 
 
