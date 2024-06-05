513f78565a9fbbb67bc842331d71b221f81135e2
nicodecillis <nicolodecillis@gmail.com>
2024-06-05 19:08:53 +0200

    Modificati gli iperparametri di decision tree

--- learning/supervised/models.py
+++ learning/supervised/models.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import GridSearchCV, train_test_split, cross_val_score
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.preprocessing import OrdinalEncoder
 
 
@@ -11,23 +11,37 @@ def decision_tree(training, target):
     tree = DecisionTreeClassifier()
     parameters_tree = {
         "criterion": ("gini", "entropy"),
+        "max_depth": (5, 10, 20),
+        "min_samples_split": (10, 20, 30),
+        "min_samples_leaf": (10, 20, 30),
+        "max_leaf_nodes": (10, 20, 30),
+        "max_features": (0.1, 0.2, 0.3)}      # valore basso per evitare l'overfitting
+
+    """
+    parameters_tree = {
+        "criterion": ("gini", "entropy"),
         "max_depth": (None, 5, 10, 20, 30),
         "min_samples_split": (2, 5, 10, 20, 30),
         "min_samples_leaf": (1, 2, 5, 10, 20, 30),
         "max_features": (0.5, "sqrt", "log2")}
+    """
 
     grid_search_tree = GridSearchCV(
         estimator=tree,
         param_grid=parameters_tree,
-        scoring="accuracy",     # sceglie modello migliore in base all'accuratezza
+        scoring="accuracy",
         n_jobs=-1,
-        cv=5,   # 5-fold cross validation (o 10?)
-        refit=True)    # refit=True: retraining del modello migliore su tutto il dataset
+        cv=5,
+        refit=True)
 
     grid_search_tree.fit(x_train, y_train)
     print("Best parameters found: ", grid_search_tree.best_params_)
     best_decision_tree = grid_search_tree.best_estimator_
-    return best_decision_tree
+    # best_decision_tree.fit(x_train, y_train)
+    y_pred = best_decision_tree.predict(x_test)
+    print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
+    print("Confusion matrix: ", metrics.confusion_matrix(y_test, y_pred))
+    print("Classification report: ", metrics.classification_report(y_test, y_pred))
 
 
 df = pd.read_csv("../../dataset/balanced-playstore-apps.csv")
@@ -42,18 +56,7 @@ categorical_features = ["App Name", "App Id", "Category", "Minimum Android", "De
 encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
 df[categorical_features] = encoder.fit_transform(df[categorical_features])
 
+
 training = df.drop("Success Rate", axis=1)
 target = df["Success Rate"]
-
-best_dtc = decision_tree(training, target)
-cross_val_score(best_dtc, training, target, cv=5)   # 5-fold cross validation (o 10?)
-confusion_matrix(target, best_dtc.predict(training))
-
-print(classification_report(target, best_dtc.predict(training)))
-print("accuracy score:" , accuracy_score(target, best_dtc.predict(training)))
-
-
-
-
-
-
+decision_tree(training, target)
